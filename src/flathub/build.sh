0ce573bde1ed1885738fc8fc7fc18a3957c6ae6f
hajdam <hajdam@users.sf.net>
2023-07-30 23:27:15 +0200

    Version raised

--- src/flathub/build.sh
+++ src/flathub/build.sh
@@ -2,3 +2,5 @@
 flatpak-builder --user --install --force-clean build-dir org.exbin.BinEd.json
 #flatpak run org.exbin.BinEd
 #flatpak remove org.exbin.BinEd
+#flatpak run org.freedesktop.appstream-glib validate org.exbin.BinEd.appdata.xml
+#desktop-file-validate org.exbin.BinEd.desktop
