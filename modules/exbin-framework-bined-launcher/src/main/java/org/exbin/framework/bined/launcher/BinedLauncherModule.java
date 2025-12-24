6d99206d5bee3b800c8c7a703d4e51e0904c92e9
hajdam <hajdam@users.sf.net>
2025-12-24 23:29:08 +0100

    Options path rework - 3

--- modules/exbin-framework-bined-launcher/src/main/java/org/exbin/framework/bined/launcher/BinedLauncherModule.java
+++ modules/exbin-framework-bined-launcher/src/main/java/org/exbin/framework/bined/launcher/BinedLauncherModule.java
@@ -106,6 +106,7 @@ public class BinedLauncherModule implements LauncherModule {
     @Override
     public void launch(String[] args) {
         OptionsModuleApi optionsModule = App.getModule(OptionsModuleApi.class);
+        optionsModule.setupAppOptions();
         OptionsStorage optionsStorage = optionsModule.getAppOptions();
         ResourceBundle bundle = App.getModule(LanguageModuleApi.class).getBundle(BinedLauncherModule.class);
 
